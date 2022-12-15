9b1fdfd9d9061f8c470cc2f03005d62d4ae9c725
marsian8 <spanarve@gmail.com>
2022-12-15 23:38:17 +0530

    changes

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ const path = require("path");
 const express = require("express");
 app = express();
 
+//Import mongoose and connect to the mongodb database
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", true);
+mongoose.connect(
+  process.env.MONGODB_CONNECTION_URI,
+  () => {
+    "Connected to mongoDB database";
+  },
+  (err) => {
+    "Could not connect to mongoDB database";
+    console.log(err);
+  }
+);
+
 //Constants
 PORT = process.env.PORT || 8000;
 NUTRITION_API = "trackapi.nutritionix.com/v2";
@@ -26,7 +40,6 @@ filesNames.forEach((filename) => {
   var router = require(route);
   app.use(`/${path.parse(route).name}`, router);
 });
-
 
 // Start the server
 app.listen(PORT, () => {
