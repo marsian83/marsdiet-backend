3e6c1d104a0ea004cddedbd294a0389c49361b7f
marsian8 <spanarve@gmail.com>
2022-12-17 01:35:58 +0530

    changes

--- routes/user.js
+++ routes/user.js
@@ -36,15 +36,6 @@ router.get("/info/:uid", async (req, res) => {
 
 router.get("/auth/info/:uid", async (req, res) => {
   try {
-    //check if the given uid is not valid
-    if (!(await User.exists({ uid: req.params.uid }))) {
-      //return a response and stop any further execution if given uid is invalid
-      return res.status(404).send({
-        message: "Something went wrong",
-        error: "The uid is not registered on the database",
-      });
-    }
-
     //get User's info from firebase
     const userInfo = await auth.getUser(req.params.uid);
 
