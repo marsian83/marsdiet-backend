44b3a38048e8783d9c8cd89cc9b3a20047349df5
marsian8 <spanarve@gmail.com>
2022-12-17 02:16:05 +0530

    changes

--- firebase.js
+++ firebase.js
@@ -8,7 +8,7 @@ const firebaseConfig = {
   type: process.env.FIREBASE_ADMIN_TYPE,
   project_id: process.env.FIREBASE_ADMIN_PROJECT_ID,
   private_key_id: process.env.FIREBASE_ADMIN_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
+  private_key: JSON.parse(process.env.FIREBASE_ADMIN_PRIVATE_KEY).privateKey,
   client_email: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
   client_id: process.env.FIREBASE_ADMIN_CLIENT_ID,
   auth_uri: process.env.FIREBASE_ADMIN_AUTH_URI,
